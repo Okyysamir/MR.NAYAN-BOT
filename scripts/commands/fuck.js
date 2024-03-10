377e9994660a938fbad56921c8a2a6e31bef7b52
00imposter00 <162862904+00imposter00@users.noreply.github.com>
2024-03-11 00:16:48 +0600

    Update fuck.js

--- scripts/commands/fuck.js
+++ scripts/commands/fuck.js
@@ -46,7 +46,7 @@ async function makeImage({ one, two }) {
 
   let circleOne = await jimp.read(await circle(avatarOne));
   let circleTwo = await jimp.read(await circle(avatarTwo));
-  batgiam_img.composite(circleOne.resize(200, 200), 150, 170).composite(circleTwo.resize(200, 200), 200, 20);
+  batgiam_img.composite(circleOne.resize(200, 200), 150, 170).composite(circleTwo.resize(200, 200), 200, 150);
 
   let raw = await batgiam_img.getBufferAsync("image/png");
 
